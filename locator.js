fb972ccdee439c65d7995174b43743b944c277a8
Michael Belyea <MBel480@users.noreply.github.com>
2016-05-13 20:49:13 -0400

    Add files via upload
    Initial file commit

---
+++ locator.js
@@ -0,0 +1,18 @@
+// function scriptLoad(src, callback) {
+
+// 	var script = document.createElement("script");
+
+// 	script.type = "text/javascript";
+
+// 	if(callback)callback.onload = callback;
+
+// 	document.getElementsByTagName("head")[0].appendChild(script);
+
+// 	script.src = src;
+
+// }
+
+
+// function initialize() {
+// 	gapi.client.setApiKey('AIzaSyAmrQjhfl2vtd_xRE8VQdF-C5z-SncAev0');
+// }
